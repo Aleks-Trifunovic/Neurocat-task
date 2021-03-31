a764499d19d5fb16047d3a7532ec790bc8275bbb
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-03-31 22:13:02 +0200

    json file created, items displayed on screen

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import items from "../items.json";
 
 class Items extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {
+    items: items
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Items: </h1>
+        {this.state.items.map((singleItem) => {
+          return <div>{singleItem.item}</div>;
+        })}
+      </div>
+    );
+  }
 }
 export default Items;
